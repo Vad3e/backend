c4ece98f2f341b32345407aeabcde8a11fd9461d
Vadeee <133876213+Vad3e@users.noreply.github.com>
2026-03-25 14:00:12 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ const upload = multer({ storage: storage });
 // ⚡ 3. BULLETPROOF EMAIL SETUP
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, 
+    port: 587,
+    secure: false, 
     auth: {
         user: process.env.EMAIL_USER,    
         pass: process.env.EMAIL_PASS     
